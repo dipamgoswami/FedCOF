792b07376048a2ceb8606e8c087bd0cb8313ec67
Dipam Goswami <40790714+dipamgoswami@users.noreply.github.com>
2026-02-03 11:45:50 +0100

    Update test_squeezenet.sh

--- bash_for_testmethods/test_squeezenet.sh
+++ bash_for_testmethods/test_squeezenet.sh
@@ -5,7 +5,7 @@ for approach in "fedncm" "fedmd" "fed3r"; do
         
         for dataset in "cifar100" "imagenet-r" "cars" "cub"; do
     
-                python -u  server_aggregate.py --dataset "$dataset" --model "squeezenet" --seed "$seed" --seed_to_load "$seed" --approach $approach --alpha_shrink 1.0
+                python -u  server_aggregate.py --dataset "$dataset" --model "squeezenet" --seed "$seed" --seed_to_load "$seed" --approach $approach --gamma_shrink 1.0
             
         done
     done
